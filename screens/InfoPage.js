74e57bd21a7892efc6bf08dd4f61b6ebe2f0f271
shema1 <shymechko.m.a@gmail.com>
2020-11-26 14:58:34 +0200

    add chart

--- screens/InfoPage.js
+++ screens/InfoPage.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-
+import { Text, View, Dimensions } from 'react-native'
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+} from "react-native-chart-kit"
 export class InfoPage extends Component {
     render() {
         return (
-           <View>
-               <Text>Info Page</Text>
-           </View>
+            <View>
+                <Text>Info Page2</Text>
+                <LineChart
+                    data={{
+                        labels: ["January", "February", "March", "April", "May", "June"],
+                        datasets: [
+                            {
+                                data: [
+                                    1,
+                                    12
+                                ]
+                            }
+                        ]
+                    }}
+                    width={Dimensions.get("window").width} // from react-native
+                    height={320}
+                    yAxisLabel="$"
+                    yAxisSuffix="k"
+                    yAxisInterval={1} // optional, defaults to 1
+                    chartConfig={{
+                        backgroundColor: "#e26a00",
+                        backgroundGradientFrom: "#fb8c00",
+                        backgroundGradientTo: "#ffa726",
+                        decimalPlaces: 2, // optional, defaults to 2dp
+                        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                        style: {
+                            borderRadius: 16
+                        },
+                        propsForDots: {
+                            r: "6",
+                            strokeWidth: "2",
+                            stroke: "#ffa726"
+                        }
+                    }}
+                    bezier
+                    style={{
+                        marginVertical: 8,
+                        borderRadius: 16
+                    }}
+                />
+            </View>
         )
     }
 }
